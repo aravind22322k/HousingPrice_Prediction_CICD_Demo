8ee8b345b5081a58cb04d0919d84e3cce2cec647
aravind22322k <aravindsg072@gmail.com>
2025-02-23 12:02:11 +0530

    Create train_model.py

---
+++ scripts/train_model.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pickle
+
+
+# Load data
+data = pd.read_csv("C:\\dat1\\housing.csv")  # Path relative to repository root
+X = data.drop("price", axis=1)
+y = data["price"]
+data.head()
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+print(X_train.head())
+
+# Train model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+
+# Save model
+with open("C:\\dat1\\models\\model.pkl", "wb") as f:  # Path relative to repository root
+    pickle.dump(model, f)
+
+
+print("Model trained and saved!")
